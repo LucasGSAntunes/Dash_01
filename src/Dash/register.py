c1b3da0bca63f0534606944a1d02d5c3ea2ecb68
LucasGSAntunes <190091681@aluno.unb.br>
2024-04-25 23:07:14 -0300

    login, register, and home

--- src/Dash/register.py
+++
@@ -1,7 +0,0 @@
-from django.forms import ModelForm
-from Dash.models import User
-
-class userForm(ModelForm):
-    class Meta:
-        model = User
-        fields = ['name', 'email', 'password']
--- src/Dash_Beta/urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.urls import path
-from Dash.views import login, register
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', login),
-    path('register/', register),
-]
